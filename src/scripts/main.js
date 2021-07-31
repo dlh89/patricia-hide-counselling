5ec2407351eec414d99dd5f8ad0cbdcd6bf21caf
David Hide <d.hide@live.co.uk>
2021-07-31 15:42:21 +0100

    Add FAQs module to what-to-expect page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
 import svgxuse from 'svgxuse'; // polyfill to load svgs from external URI (unsupported by IE)
 import Navigation from './modules/nav';
+import Faqs from './modules/faqs';
 
 const navigation = new Navigation();
+const faqs = new Faqs();
---
+++ src/scripts/modules/faqs.js
@@ -0,0 +1,29 @@
+export default class Navigation {
+    constructor() {
+      const questions = document.querySelectorAll('.js-faq-question');
+
+      // Give the first answer a px height as it starts open and transition won't work on non px value
+      const answers = document.querySelectorAll('.js-faq-answer');
+      answers[0].style.maxHeight = `${answers[0].scrollHeight}px`;
+
+      questions.forEach((question) => {
+        question.addEventListener('click', () => {
+            this.hideActiveQuestions();
+            const associatedAnswer = question.nextElementSibling;
+            associatedAnswer.classList.add('faqs__answer--active');
+            associatedAnswer.style.maxHeight = `${associatedAnswer.scrollHeight}px`;
+            associatedAnswer.setAttribute('aria-hidden', 'false');
+        });
+      });
+    }
+
+    hideActiveQuestions() {
+      const answers = document.querySelectorAll('.js-faq-answer');
+
+      answers.forEach(function(answer) {
+        answer.style.maxHeight = 0;
+        answer.setAttribute('aria-hidden', 'true');
+      });
+    }
+  }
+  
