35c9795693e52112e55945213a7defd7b1e3f1be
David Hide <d.hide@live.co.uk>
2025-04-05 18:01:29 +0100

    Separate scripts for development and production

--- webpack.config.js
+++ webpack.config.js
@@ -8,51 +8,53 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/scripts/main.js', // create this file if you don't have one
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: 'about/index.html',
-      filename: 'about/index.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: 'contact/index.html',
-      filename: 'contact/index.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: 'what-to-expect/index.html',
-      filename: 'what-to-expect/index.html',
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname),
+module.exports = (env, argv) => {
+  return {
+    mode: argv.mode || 'development',
+    entry: './src/scripts/main.js', // create this file if you don't have one
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
     },
-    compress: true,
-    port: 9000,
-  },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader'
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+      }),
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+      }),
+      new HtmlWebpackPlugin({
+        template: 'about/index.html',
+        filename: 'about/index.html',
+      }),
+      new HtmlWebpackPlugin({
+        template: 'contact/index.html',
+        filename: 'contact/index.html',
+      }),
+      new HtmlWebpackPlugin({
+        template: 'what-to-expect/index.html',
+        filename: 'what-to-expect/index.html',
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname),
+      },
+      compress: true,
+      port: 9000,
+    },
+  }
 };
